4036591fee64e7b945af855a2fc49b9cb2fd4aba
Alex <mr.tal44@yandex.ru>
2022-11-13 19:05:41 +0300

    add If Elsa

---
+++ ex005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "света")  
+{
+      Console.Write("Ура!!! Это Света");  
+}   
+else
+{
+        Console.Write("Привет, ");
+        Console.Write(username);
+}
